bc415c853f80925cbbc5fa394dee83fcd59b022a
pemrouz <p3pedram@hotmail.com>
2016-07-31 12:21:15 +0100

    feat: only retry after failure

--- src/index.js
+++ src/index.js
@@ -107,29 +107,31 @@ export default function popper({
 
   function ci(r) {
     if (!isCI || r.stats.running) return
-    if (r.retries < maxRetries) 
+
+    const browser = browsers
+      .filter(d => {
+        if (d._name       && d._name       !== r.platform.name)        return false
+        if (d._version    && d._version    !== r.platform.version)     return false
+        if (d._os         && d._os         !== r.platform.os.name)     return false
+        if (d._os_version && d._os_version !== r.platform.os.version)  return false
+        return true
+      })
+      .pop()
+
+    if (!browser) return log('result not in matrix'.red, r.platform.uid)
+
+    browser.passed_by = r.platform.uid
+    browser.passed    = !r.stats.failures
+    browser.passed
+      ? log('browser passed:', r.platform.uid.green.bold)
+      : err('browser failed:', r.platform.uid.red.bold)
+
+    if (!browser.passed && r.retries < maxRetries) 
       return log('retrying'.yellow, r.platform.uid, ++r.retries, '/', str(maxRetries).grey) 
            , reload(r.platform.uid)
 
-    browsers
-      .filter(d => {
-        if (d.passed)                                                 return false
-        if (d._name       && d._name       !== r.platform.name)       return false
-        if (d._version    && d._version    !== r.platform.version)    return false
-        if (d._os         && d._os         !== r.platform.os.name)    return false
-        if (d._os_version && d._os_version !== r.platform.os.version) return false
-        return true
-      })
-      .map(d => {
-        d.passed_by = r.platform.uid
-        d.passed    = !r.stats.failures
-        d.passed
-          ? log('browser passed:', r.platform.uid.green.bold)
-          : err('browser failed:', r.platform.uid.red.bold)
-
-        if (farms[farm].status)
-          farms[farm].status(d, r.platform)
-      })
+    if (farms[farm].status)
+      farms[farm].status(browser, r.platform)
 
     const target   = browsers.length
         , passed   = browsers.filter(by('passed')).length
