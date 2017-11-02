c5d60ca2e792dbea1a39566ce12089f8c6c28731
pemrouz <p3pedram@hotmail.com>
2017-11-02 22:54:24 +0000

    major: refactor

--- src/farms/index.js
+++
@@ -1,4 +0,0 @@
-import browserstack from './browserstack'
-import saucelabs from './saucelabs'
-
-export default { browserstack, saucelabs }
--- src/resources/logs-view/logs-view.js
+++
@@ -1,7 +0,0 @@
-export default function logsView(node, { results }){
-  const id = location.pathname.split('dashboard/').pop().replace(/-$/, '-?')
-  
-  raw('#output').innerHTML = results[id] 
-    ? results[id].html 
-    : 'No connected agent yet..'
-}
